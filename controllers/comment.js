a8f8d3c194b5ca19e4f33cd5fbb23afa1599eb3c
NicoOjeda <nico_ojeda86@hotmail.com>
2024-07-13 20:40:51 -0300

    se define controlador para crear comentarios

---
+++ controllers/comment.js
@@ -0,0 +1,51 @@
+const Comment = require('../models/Comment')
+
+const controller = {
+
+    create: async(req,res)=>{
+
+
+
+        try{
+            let new_comment = await Comment.create(req.body)
+
+            res.status(201).json({
+                id: new_comment._id,
+                success: true,
+                message: 'comentario creado'
+            })
+        } catch(error){
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+
+    read: async(req,res)=>{
+        try{
+            
+        } catch(){
+            
+        }
+    },
+
+    update: async(req,res)=>{
+        try{
+
+        } catch(){
+            
+        }
+    },
+
+    destroy: async(req,res)=>{
+        try{
+
+        } catch(){
+            
+        }
+    },
+
+}
+
+module.exports = controller
