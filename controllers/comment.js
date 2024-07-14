aa379e1ec63a4c7fcbe552a5c1d3c5f483d53739
NicoOjeda <nico_ojeda86@hotmail.com>
2024-07-14 20:46:54 -0300

    se definen metodos para eliminar, leer y modificar

--- controllers/comment.js
+++ controllers/comment.js
@@ -19,29 +19,75 @@ const controller = {
         }
     },
 
-    // read: async(req,res)=>{
-    //     try{
-            
-    //     } catch(){
-            
-    //     }
-    // },
+    read: async(req,res)=>{
+        try{
+            let todos = await Comment.find()
 
-    // update: async(req,res)=>{
-    //     try{
+            res.status(200).json({
+                response: todos,
+                success: true,
+                message: 'Se encontraron comentarios'
+            })
+            console.log(todos);
+        } catch(error){
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
 
-    //     } catch(){
+    update: async(req,res)=>{
+        let{id} = req.params
+
+        try{
+            let uno = await Comment.findOneAndUpdate({_id: id}, req.body, {new: true})
             
-    //     }
-    // },
-
-    // destroy: async(req,res)=>{
-    //     try{
-
-    //     } catch(){
+            if(uno){
+                res.status(200).json({
+                    id: uno._id,
+                    success: true,
+                    message: 'Se modifico comentario'
+                })
+            } else{
+                res.status(404).json({
+                    success: false,
+                    message: 'El comentario no existe'
+                })
+            }
             
-    //     }
-    // }
+        } catch(error){
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    },
+
+    destroy: async(req,res)=>{
+        let{id} = req.params
+
+        try{
+            let uno = await Comment.findOneAndDelete({_id: id})
+            if(uno){
+                res.status(200).json({
+                    id: uno._id,
+                    success: true,
+                    message: 'Se elimino comentario'
+                })
+            } else{
+                res.status(404).json({
+                    success: false,
+                    message: 'El comentario no existe'
+                })
+            }
+        } catch(error){
+            res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
 
 }
 
